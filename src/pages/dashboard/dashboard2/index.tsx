6ced73cbe7c00ef30da08a2f3f1e5781cb83a392
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2024-10-09 18:23:26 +0200

    Update index.tsx

--- src/pages/dashboard/dashboard2/index.tsx
+++ src/pages/dashboard/dashboard2/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
+import join from "lodash/join";
 
 export default function Dashboard() {
 	const [data, setData] = useState<string | null>(null);
 
 	useEffect(() => {
 		const loadLibrary = async () => {
-			const { join } = await import("lodash/join");
 			const result = join(["Hello", "World"], " ");
 			setData(result);
 		};
